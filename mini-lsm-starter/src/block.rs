3d040c70e8b493d5fbd7a80ee84a9446c10e002e
Supasin Liulak <palsp2406@gmail.com>
2026-08-22 00:01:58 +0700

    week1 day3: block builder

--- mini-lsm-starter/src/block.rs
+++ mini-lsm-starter/src/block.rs
@@ -19,8 +19,10 @@ mod builder;
 mod iterator;
 
 pub use builder::BlockBuilder;
-use bytes::Bytes;
+use bytes::{BufMut, Bytes, BytesMut};
 pub use iterator::BlockIterator;
+
+use crate::key::Key;
 
 /// A block is the smallest unit of read and caching in LSM tree. It is a collection of sorted key-value pairs.
 pub struct Block {
@@ -32,11 +34,49 @@ impl Block {
     /// Encode the internal data to the data layout illustrated in the course
     /// Note: You may want to recheck if any of the expected field is missing from your output
     pub fn encode(&self) -> Bytes {
-        unimplemented!()
+        let mut out = BytesMut::new();
+
+        let mut i: usize = 0;
+        for j in 1..self.offsets.len() {
+            let end = self.offsets[j] as usize;
+            out.put_slice(&self.data[i..end]);
+            i = end;
+        }
+
+        out.put_slice(&self.data[i..self.data.len()]);
+
+        for &i in self.offsets.iter() {
+            out.put_u16(i);
+        }
+
+        out.put_u16(self.offsets.len() as u16);
+
+        out.freeze()
     }
 
     /// Decode from the data layout, transform the input `data` to a single `Block`
     pub fn decode(data: &[u8]) -> Self {
-        unimplemented!()
+        let len = data.len();
+        let num_of_elements = u16::from_be_bytes(data[len - 2..len].try_into().unwrap()) as usize;
+
+        let mut builder = BlockBuilder::new(65_535);
+
+        let mut i = 0;
+        for _ in 0..num_of_elements {
+            let key_offset = i + 2;
+            let key_len = u16::from_be_bytes(data[i..key_offset].try_into().unwrap()) as usize;
+            let key = &data[key_offset..key_offset + key_len];
+            let value_len_offset = key_offset + key_len;
+            let value_offset = value_len_offset + 2;
+            let value_len =
+                u16::from_be_bytes(data[value_len_offset..value_offset].try_into().unwrap())
+                    as usize;
+            let value = &data[value_offset..value_offset + value_len];
+
+            assert!(builder.add(Key::from_slice(key), value), "block overflow");
+            i = value_offset + value_len;
+        }
+
+        builder.build()
     }
 }
--- mini-lsm-starter/src/block/builder.rs
+++ mini-lsm-starter/src/block/builder.rs
@@ -15,6 +15,8 @@
 #![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
+use bytes::{BufMut, BytesMut};
+
 use crate::key::{KeySlice, KeyVec};
 
 use super::Block;
@@ -34,23 +36,46 @@ pub struct BlockBuilder {
 impl BlockBuilder {
     /// Creates a new block builder.
     pub fn new(block_size: usize) -> Self {
-        unimplemented!()
+        BlockBuilder {
+            offsets: Vec::new(),
+            data: Vec::new(),
+            block_size,
+            first_key: KeyVec::new(),
+        }
     }
 
     /// Adds a key-value pair to the block. Returns false when the block is full.
     /// You may find the `bytes::BufMut` trait useful for manipulating binary data.
     #[must_use]
     pub fn add(&mut self, key: KeySlice, value: &[u8]) -> bool {
-        unimplemented!()
+        let offset = self.data.len();
+        let mut data = BytesMut::new();
+
+        data.put_u16(key.len() as u16);
+        data.put_slice(key.into_inner());
+        data.put_u16(value.len() as u16);
+        data.put_slice(value);
+
+        let size = self.data.len() + self.offsets.len();
+        if size != 0 && size + data.len() + 2 > self.block_size {
+            return false;
+        }
+
+        self.data.append(&mut data.to_vec());
+        self.offsets.push(offset.try_into().unwrap());
+        true
     }
 
     /// Check if there is no key-value pair in the block.
     pub fn is_empty(&self) -> bool {
-        unimplemented!()
+        self.data.is_empty()
     }
 
     /// Finalize the block.
     pub fn build(self) -> Block {
-        unimplemented!()
+        Block {
+            data: self.data,
+            offsets: self.offsets,
+        }
     }
 }
