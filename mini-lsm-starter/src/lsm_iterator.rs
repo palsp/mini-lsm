fa13b13ef4e63fbe01d6ac47984e40c3a9a4b94c
Supasin Liulak <palsp2406@gmail.com>
2026-08-28 22:55:35 +0700

    implement num_active_iterators

--- mini-lsm-starter/src/lsm_iterator.rs
+++ mini-lsm-starter/src/lsm_iterator.rs
@@ -88,6 +88,10 @@ impl StorageIterator for LsmIterator {
 
         Ok(())
     }
+
+    fn num_active_iterators(&self) -> usize {
+        self.inner.num_active_iterators()
+    }
 }
 
 /// A wrapper around existing iterator, will prevent users from calling `next` when the iterator is
@@ -138,5 +142,9 @@ impl<I: StorageIterator> StorageIterator for FusedIterator<I> {
         }
 
         Ok(())
+    }
+
+    fn num_active_iterators(&self) -> usize {
+        self.iter.num_active_iterators()
     }
 }
